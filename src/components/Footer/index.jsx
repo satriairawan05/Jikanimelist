4242d4d0a8e7f59c585b3360cf9652d2c9e9741a
satriairawan05 <satriai418@gmail.com>
2023-12-12 11:30:17 +0800

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,9 +2,9 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <>
-      <footer className="flex justify-around py-3 my-5 text-anime-primary">
-        <div className="items-center">
-          <h1 className="text-2xl">&copy; <a href="https://github.com/satriairawan05" target="_blank">Deuwi Satriya Irawan</a> 2023 - {currentYear}</h1>
+      <footer className="flex justify-around text-anime-primary">
+        <div className="items-center py-3 my-5">
+          <h1 className="text-xl text-center lg:text-2xl">&copy; 2023 - {currentYear} Distributed by <a href="https://github.com/satriairawan05" target="_blank">Deuwi Satriya Irawan</a></h1>
         </div>
       </footer>
     </>
