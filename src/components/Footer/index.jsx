05948de3e7032819588016d7db9504148af63c2c
satriairawan05 <satriai418@gmail.com>
2023-12-16 08:38:37 +0800

    Fix Typo in Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,9 +5,9 @@ const Footer = ({ currentYear, link, name }) => {
     <>
       <footer className="flex justify-around text-anime-primary">
         <div className="items-center py-3 my-5">
-          <p className="text-center">
+          <p className="text-sm text-center lg:text-md xl:text-2xl">
             © 2023 - {currentYear} Distributed by{" "}
-            <Link href={link} target="_blank" key={name}>
+            <Link href={link} target="_blank" key={name} className="hover:text-anime-accent">
               {name}
             </Link>
           </p>
