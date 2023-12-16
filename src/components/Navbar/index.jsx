2902e93c7b2fe87f45ce00eae120991cb1e8daba
satriairawan05 <satriai418@gmail.com>
2023-12-16 14:21:17 +0800

    Fix Responsive

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,7 @@ import InputSearch from "./InputSearch";
 const Navbar = () => {
   return (
     <header className="bg-anime-accent">
-      <div className="flex flex-col justify-between gap-2 p-4 md:flex-row md:items-center sm:text-center">
+      <div className="flex flex-col justify-between gap-2 p-4 md:flex-row md:items-center">
         <Link href="/" className="text-2xl font-bold uppercase text-anime-primary">
           Jikanimelist
         </Link>
