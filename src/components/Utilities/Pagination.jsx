ff665f54fa8ef43853f26ecfa88a3e9d06dad241
satriairawan05 <satriai418@gmail.com>
2023-12-22 09:59:06 +0800

    Remove Lg Pagination

--- src/components/Utilities/Pagination.jsx
+++ src/components/Utilities/Pagination.jsx
@@ -24,7 +24,7 @@ const Pagination = ({ page, lastPage, setPage }) => {
       <div className="inline-flex mt-1 xs:mt-0">
         {page <= 1 ? null : (
           <>
-            <button onClick={handlePrevPage} className="flex items-center justify-center h-8 px-3 transition-all border rounded-lg lg:h-10 lg:px-4 hover:text-anime-accent">
+            <button onClick={handlePrevPage} className="flex items-center justify-center h-8 px-3 transition-all border rounded-lg hover:text-anime-accent">
               <svg class="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4" />
               </svg>
@@ -33,7 +33,7 @@ const Pagination = ({ page, lastPage, setPage }) => {
           </>
         )}
         {page >= lastPage ? null : (
-          <button onClick={handleNextPage} className="flex items-center justify-center h-8 px-3 transition-all border rounded-lg lg:h-10 lg:px-4 hover:text-anime-accent">
+          <button onClick={handleNextPage} className="flex items-center justify-center h-8 px-3 transition-all border rounded-lg hover:text-anime-accent">
             Next
             <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
