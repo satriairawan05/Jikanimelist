d15637d5f184b3360f42a5d001e8cc06d37445fa
satriairawan05 <satriai418@gmail.com>
2023-12-22 09:12:09 +0800

    fix Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 import { getGithubUser } from "@/libs/api";
 import Link from "next/link";
 
-const Footer = async({ currentYear, link, name }) => {
+const Footer = async({ currentYear, link }) => {
   const user = await getGithubUser("satriairawan05")
   return (
     <>
@@ -9,7 +9,7 @@ const Footer = async({ currentYear, link, name }) => {
         <div className="items-center py-3 my-5">
           <p className="text-sm text-center lg:text-md xl:text-2xl">
             © 2023 - {currentYear} Distributed by{" "}
-            <Link href={link} target="_blank" key={name} className="hover:text-anime-accent">
+            <Link href={link} target="_blank" key={user.name} className="hover:text-anime-accent">
               {user.name}
             </Link>
           </p>
