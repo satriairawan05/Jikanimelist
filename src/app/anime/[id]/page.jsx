52d6341bf0e2d8c473efe00f948cbbe398abd6cf
satriairawan05 <satriai418@gmail.com>
2023-12-23 11:48:52 +0800

    Fix 1 show anime

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -8,32 +8,32 @@ const Page = async ({ params: { id } }) => {
 
   return (
     <>
-      <div className="flex gap-2 px-4 pt-4 overflow-x-auto">
-        <div className="flex flex-col items-center justify-center p-2 border rounded w-36 border-anime-primary text-anime-primary">
+      <div className="flex gap-2 px-4 pt-4 overflow-x-auto border-anime-primary text-anime-primary">
+        <div className="flex flex-col items-center justify-center p-2 border rounded w-36">
           <h3 className="text-uppercase">Peringkat</h3>
           <p>{anime.data.rank}</p>
         </div>
-        <div className="flex flex-col items-center justify-center p-2 border rounded w-36 border-anime-primary text-anime-primary">
+        <div className="flex flex-col items-center justify-center p-2 border rounded w-36">
           <h3 className="text-uppercase">Skor</h3>
           <p>{anime.data.score}</p>
         </div>
-        <div className="flex flex-col items-center justify-center p-2 border rounded w-36 border-anime-primary text-anime-primary">
+        <div className="flex flex-col items-center justify-center p-2 border rounded w-36">
           <h3 className="text-uppercase">Anggota</h3>
           <p>{anime.data.members}</p>
         </div>
-        <div className="flex flex-col items-center justify-center p-2 border rounded w-36 border-anime-primary text-anime-primary">
+        <div className="flex flex-col items-center justify-center p-2 border rounded w-36">
           <h3 className="text-uppercase">Episode</h3>
           <p>{anime.data.episodes}</p>
         </div>
-        <div className="flex flex-col items-center justify-center p-2 border rounded w-36 border-anime-primary text-anime-primary">
+        <div className="flex flex-col items-center justify-center p-2 border rounded w-36">
           <h3 className="text-uppercase">Watching</h3>
           <p>{animeStatistics.data.watching}</p>
         </div>
-        <div className="flex flex-col items-center justify-center p-2 border rounded w-36 border-anime-primary text-anime-primary">
+        <div className="flex flex-col items-center justify-center p-2 border rounded w-36">
           <h3 className="text-uppercase">Completed</h3>
           <p>{animeStatistics.data.completed}</p>
         </div>
-        <div className="flex flex-col items-center justify-center p-2 border rounded w-36 border-anime-primary text-anime-primary">
+        <div className="flex flex-col items-center justify-center p-2 border rounded w-36">
           <h3 className="text-uppercase">Total</h3>
           <p>{animeStatistics.data.total}</p>
         </div>
